fe72ad9de823ac1b7f23a8a1f5d4de10882b4ac4
Ahsan Khan <asifuddinahmed123@gmail.com>
2021-10-11 11:11:32 +0600

    refactorization (incomplete - sample3)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 883, 600));
         primaryStage.show();
