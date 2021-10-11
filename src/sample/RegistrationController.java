7cd4a686011dad6a3c95e0694105bac0fe15d5e0
Ahsan Khan <asifuddinahmed123@gmail.com>
2021-10-11 12:48:04 +0600

    added error msg

--- src/sample/RegistrationController.java
+++ src/sample/RegistrationController.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class RegistrationController {
@@ -24,6 +25,9 @@ public class RegistrationController {
 
     @FXML
     private Button Register;
+
+    @FXML
+    private Label errorMsg;
 
     @FXML
     void userRegister(ActionEvent event) {
