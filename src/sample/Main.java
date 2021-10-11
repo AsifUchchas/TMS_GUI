bf4f1a7ca5c02c796f5fcdab4ab62bc1582ac99e
Ahsan Khan <asifuddinahmed123@gmail.com>
2021-10-11 14:05:39 +0600

    added chengeScene method

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Registration.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 883, 600));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        changeScene(primaryStage, "Registration.fxml");
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void changeScene(Stage primaryStage, String fxmlName) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource(fxmlName)));
+        primaryStage.setTitle("Tourist Management System");
+        primaryStage.setScene(new Scene(root, 883, 600));
+        primaryStage.show();
     }
 }
