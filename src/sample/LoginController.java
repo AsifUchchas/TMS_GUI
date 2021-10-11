03dab1098ff6ded39ecbfad2bcbad8e0c5369aea
Ahsan Khan <asifuddinahmed123@gmail.com>
2021-10-11 11:16:23 +0600

    refactorization (p2)

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class Controller2 {
+public class LoginController {
 
     @FXML
     private TextField userLoginMail;
--- src/sample/RegistrationController.java
+++ src/sample/RegistrationController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class Controller {
+public class RegistrationController {
 
     @FXML
     private TextField userName;
