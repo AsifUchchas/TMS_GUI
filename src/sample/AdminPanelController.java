4cbcd60323d00859391c2a7e7c5b7523dd4ecded
AsifUchchas <asifuddinahmed123@gmail.com>
2021-10-12 13:14:20 +0600

    Added admin panel and controller

---
+++ src/sample/AdminPanelController.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AdminPanelController {
+
+    @FXML
+    private TextField placeName;
+
+    @FXML
+    private TextField numberOfPeople;
+
+    @FXML
+    void manipulateInformation(ActionEvent event) {
+
+    }
+
+}
+
