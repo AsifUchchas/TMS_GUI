71e1a027d16e58d7504f4735d0d57a5a66c47dbb
rafin201175 <83057929+rafin201175@users.noreply.github.com>
2021-10-12 15:13:13 +0600

    Updated Admin Controller

--- src/sample/AdminPanelController.java
+++ src/sample/AdminPanelController.java
@@ -4,6 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+
 public class AdminPanelController {
 
     @FXML
@@ -14,7 +21,34 @@ public class AdminPanelController {
 
     @FXML
     void manipulateInformation(ActionEvent event) {
+        HashMap<String, Integer> place = new HashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/sample/place_population"));
+            String line1;
+            String line2;
+            while ((line1 = br.readLine()) != null) {
+                line2 = br.readLine();
+                place.put(line1, Integer.parseInt(line2));
+            }
+            br.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        place.put(placeName.getText(), Integer.parseInt(numberOfPeople.getText()));
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/sample/place_population"));
+            for (String key : place.keySet()) {
+                bw.write(key + "\n");
+                bw.write(place.get(key) + "\n");
+            }
+            bw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
