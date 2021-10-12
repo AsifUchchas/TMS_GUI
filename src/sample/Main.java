e8ede2219c2c274ad02ef4125349f0664954e968
Ahsan Khan <asifuddinahmed123@gmail.com>
2021-10-12 11:52:40 +0600

    change start page

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        changeScene(primaryStage, "RegistrationScene.fxml");
+        changeScene(primaryStage, "LoginScene.fxml");
     }
 
 
